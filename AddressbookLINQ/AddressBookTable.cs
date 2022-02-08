2befc048d7b5a3e6cf69dfdec67d187f98052f6f
Your Name <akhilkumarchandragiri@gmail.com>
2022-02-08 22:00:07 +0530

    [Master] uc1

---
+++ AddressbookLINQ/AddressBookTable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace AddressbookLINQ
+{
+    class AddressBookTable
+    {
+        DataTable table = new DataTable("AddressBookSystem");
+    }
+}
