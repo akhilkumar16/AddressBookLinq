c1e6ba2471e0d2095806c1ab5040e76c4a5ab60c
Your Name <akhilkumarchandragiri@gmail.com>
2022-02-08 22:01:54 +0530

    [Master] uc2

--- AddressbookLINQ/AddressBookTable.cs
+++ AddressbookLINQ/AddressBookTable.cs
@@ -8,5 +8,16 @@ namespace AddressbookLINQ
     class AddressBookTable
     {
         DataTable table = new DataTable("AddressBookSystem");
+        public AddressBookTable()
+        {
+            table.Columns.Add("FirstName", typeof(string));  
+            table.Columns.Add("LastName", typeof(string));
+            table.Columns.Add("Address", typeof(string));
+            table.Columns.Add("City", typeof(string));
+            table.Columns.Add("State", typeof(string));
+            table.Columns.Add("Zip", typeof(string));
+            table.Columns.Add("PhoneNumber", typeof(string));
+            table.Columns.Add("Email", typeof(string));
+        }
     }
 }
